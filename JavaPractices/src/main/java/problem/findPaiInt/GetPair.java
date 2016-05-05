1f596702ecaafbddd6f7da1456043c8aa4d67aae
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-06 00:02:31 +0200

    ignored

--- JavaPractices/src/main/java/problem/findPaiInt/GetPair.java
+++ JavaPractices/src/main/java/problem/findPaiInt/GetPair.java
@@ -1,7 +1,30 @@
 package problem.findPaiInt;
 
+import java.util.Scanner;
+
 /**
  * Created by shantonu on 4/12/16.
  */
 public class GetPair {
+
+    public static void main(String[] args)  {
+
+        Scanner inp =new Scanner(System.in);
+        try {
+            while(true) {
+                int a = inp.nextInt();
+                int b = inp.nextInt();
+                    System.out.println("X = " + (a + b));
+                }
+            }
+            catch (Exception ex)
+            {
+               //this will be a very very long exception due to input chain events
+            }finally {
+                inp.close();
+            }
+
+
+
+    }
 }
