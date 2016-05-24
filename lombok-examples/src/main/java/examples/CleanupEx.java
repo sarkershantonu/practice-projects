f77a98afc7637ca954d2211f8181463f6168e767
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-24 03:04:41 +0200

    added cleanup and val

--- lombok-examples/src/main/java/examples/CleanupEx.java
+++ lombok-examples/src/main/java/examples/CleanupEx.java
@@ -1,7 +1,21 @@
 package examples;
 
+import lombok.Cleanup;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 /**
  * Created by shantonu on 5/18/16.
  */
 public class CleanupEx {
+
+    public void check() throws FileNotFoundException {
+        @Cleanup
+        FileInputStream fis = new FileInputStream("/Doc/Guide");
+        @Cleanup
+        FileOutputStream fos = new FileOutputStream("/Doc/Out");
+
+    }
 }
--- lombok-examples/src/main/java/examples/ValEx.java
+++ lombok-examples/src/main/java/examples/ValEx.java
@@ -3,13 +3,25 @@ package examples;
 import lombok.val;
 
 import java.util.Hashtable;
-import java.util.Objects;
 
 /**
  * Created by shantonu on 5/18/16.
  */
 public class ValEx {
-    public void aMethod() {
-        val example = new Hashtable<String, Objects>();
+    public void  aMethod() {
+        val store = new Hashtable<String, String>();
+
+        store.put("one", new String("Shantonu"));
+
+        val func = store.get("one");
+        System.out.println(func.toLowerCase());
+
+        store.put("two", "Andrii");
+        store.put("three", "Oleks");
+
+        for(val v: store.entrySet()){
+            System.out.println("KEY =" +v.getKey()+", VAL = "+ v.getValue());
+        }
+
     }
 }
