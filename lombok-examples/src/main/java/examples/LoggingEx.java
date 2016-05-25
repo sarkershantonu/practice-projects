298ba4120d340fb79e6bbe7f4da31c357264b32c
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-25 07:06:56 +0200

    logging with lombok

---
+++ lombok-examples/src/main/java/examples/LoggingEx.java
@@ -0,0 +1,22 @@
+package examples;
+
+import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+
+/**
+ * Created by shantonu on 5/25/16.
+ */
+
+@Log
+@Slf4j
+@Log4j
+@Log4j2
+@XSlf4j
+@CommonsLog
+public class LoggingEx {
+
+}
