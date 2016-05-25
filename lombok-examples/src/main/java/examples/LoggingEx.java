37b5e6535c6e59aa7076a5133bf7d9fc01f98c6f
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-25 08:18:18 +0200

    logging with lombok

--- lombok-examples/src/main/java/examples/LoggingEx.java
+++ lombok-examples/src/main/java/examples/LoggingEx.java
@@ -1,6 +1,4 @@
 package examples;
-
-
 import lombok.extern.apachecommons.CommonsLog;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
@@ -9,16 +7,20 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.extern.slf4j.XSlf4j;
 
 import java.util.logging.Level;
-
 /**
  * Created by shantonu on 5/25/16.
  */
 @Slf4j
 public class LoggingEx {
     public void slf4j() {
-
     }
-    public static void main(String... args) {
+
+}
+
+@XSlf4j
+class LoggingEx1 {
+    public void xslf4j() {
+
 
     }
 }
@@ -51,14 +53,6 @@ class LoggingEx4 {
 @Log4j2
 class LoggingEx5 {
     public void log4j2() {
-
-    }
-}
-
-@XSlf4j
-class LoggingEx1 {
-    public void xslf4j() {
-
 
     }
 }
