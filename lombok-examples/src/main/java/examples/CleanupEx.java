5fc0533368ef31b994b9e0a0c83a1be92568ee61
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-31 03:18:24 +0200

    refactored

--- lombok-examples/src/main/java/examples/CleanupEx.java
+++ lombok-examples/src/main/java/examples/CleanupEx.java
@@ -9,23 +9,13 @@ import java.io.*;
  */
 public class CleanupEx {
 
-    public void check() throws FileNotFoundException, IOException {
-        @Cleanup
-        FileInputStream fis = new FileInputStream("/Doc/Guide");
-        @Cleanup
-        FileOutputStream fos = new FileOutputStream("/Doc/Out");
-
-    }
-
     public static void main(String[] args) throws IOException {
         String pom = System.getProperty("user.dir") + "/pom.xml";
         String io  = System.getProperty("user.dir") + "/logs/io.txt";
         @Cleanup InputStream in = new FileInputStream(pom);
         @Cleanup OutputStream out = new FileOutputStream(io);
-
         int reading;
         char c;
-
         while ((reading=in.read())!=-1){
             c=(char)reading;
             out.write(c);
