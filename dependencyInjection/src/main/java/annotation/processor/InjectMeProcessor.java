582ec09c8e25468e8ab11a32763ea621b22908a5
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-17 23:51:01 +0200

    annotation processing

--- dependencyInjection/src/main/java/annotation/processor/InjectMeProcessor.java
+++ dependencyInjection/src/main/java/annotation/processor/InjectMeProcessor.java
@@ -1,5 +1,10 @@
 package annotation.processor;
 
+import sun.reflect.annotation.AnnotationType;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
 /**
  * Created by shantonu on 6/16/16.
  * todo => reflection base item invocation and processing
@@ -7,4 +12,14 @@ package annotation.processor;
 
 public class InjectMeProcessor {
 
+    public static void inject(Class<?> aClass)
+    {
+        String packageinfo = aClass.getPackage().getName();
+        for(Field field : aClass.getDeclaredFields()) {
+            for (Annotation type : field.getDeclaredAnnotations())
+            {
+                
+            }
+        }
+    }
 }
