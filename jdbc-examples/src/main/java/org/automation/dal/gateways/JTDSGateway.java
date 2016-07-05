96d4a2fc2c8b99a6c1bc2d013e32a1687f0a5ecf
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-07-05 19:27:27 +0200

    Update JTDSGateway.java
    JTDS sybase

--- jdbc-examples/src/main/java/org/automation/dal/gateways/JTDSGateway.java
+++ jdbc-examples/src/main/java/org/automation/dal/gateways/JTDSGateway.java
@@ -3,5 +3,11 @@ package org.automation.dal.gateways;
 /**
  * Created by shantonu on 7/5/16.
  */
-public class JTDSGateway {
+class JTDSGateway {
+    public static final String url = "jdbc:jtds:sybase://<host>:<ip>/<dbName>";
+    public static final String urlLargeLinit = "jdbc:jtds:sybase://<host>:<ip>/<dbName>;batchSize=10000";
+    public static synchronized Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new net.sourceforge.jtds.jdbc.Driver());
+        return DriverManager.getConnection(url, "MFILES5", "Mfiles5");
+    }
 }
