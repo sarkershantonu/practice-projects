abbf59bc8b1a1ce7fb7364afba438c0eb2a521b6
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-07-05 19:31:04 +0200

    Update JconnectGateway.java
    updated jconnect

--- jdbc-examples/src/main/java/org/automation/dal/gateways/JconnectGateway.java
+++ jdbc-examples/src/main/java/org/automation/dal/gateways/JconnectGateway.java
@@ -3,5 +3,13 @@ package org.automation.dal.gateways;
 /**
  * Created by shantonu on 7/5/16.
  */
-public class JconnectGateway {
+class JconnectGateway {
+    public static String url = "jdbc:sybase:Tds:<host>:<port>/<db>";
+    public static Connection getConnection() throws SQLException {
+       return  DriverManager.getConnection("jdbc:sybase:Tds:<host>:<port>", "user", "pass");
+    }
+    public static Connection getConn() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        SybDriver driver = (SybDriver) Class.forName("com.sybase.jdbc3.jdbc.SybDriver").newInstance();
+        return DriverManager.getConnection("jdbc:sybase:Tds:<host>:<port>", "user", "pass");
+    }
 }
