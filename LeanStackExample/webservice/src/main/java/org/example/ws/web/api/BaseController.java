ca328d4c53c835b6255323bbf90b143328972c09
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-07-06 18:40:36 +0200

    Update BaseController.java
    remove errors

--- LeanStackExample/webservice/src/main/java/org/example/ws/web/api/BaseController.java
+++ LeanStackExample/webservice/src/main/java/org/example/ws/web/api/BaseController.java
@@ -17,7 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import antlr.collections.List;
+import java.util.*;
 
 public class BaseController {
 	
