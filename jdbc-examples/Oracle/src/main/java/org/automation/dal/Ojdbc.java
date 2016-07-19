03a806aaafce128ece3320668fef9b08a2ebcc8e
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-19 23:52:35 +0200

    POM added for odbc

---
+++ jdbc-examples/Oracle/src/main/java/org/automation/dal/Ojdbc.java
@@ -0,0 +1,25 @@
+package org.automation.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by shantonu on 7/5/16.
+ */
+public class Ojdbc {
+
+    public static String url= "jdbc:mysql://<HOST>:<PORT>/<DB>";
+    private static final String user = "user";
+    private static final String pass = "pass";
+    private static final String className = "com.mysql.jdbc.Driver";
+
+    public static synchronized Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new );
+        return DriverManager.getConnection(url, user, pass);
+    }
+    public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class.forName(className).newInstance();
+        return DriverManager.getConnection(url,user,pass);
+    }
+}
