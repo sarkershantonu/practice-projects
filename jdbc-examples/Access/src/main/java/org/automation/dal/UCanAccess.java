465e794c92beea7623bf1898bea3a29ebe9fca19
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-04 00:33:29 +0200

    UCanAcces Example for connection

---
+++ jdbc-examples/Access/src/main/java/org/automation/dal/UCanAccess.java
@@ -0,0 +1,24 @@
+package org.automation.dal;
+
+import net.ucanaccess.jdbc.UcanaccessDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by shantonu on 8/4/16.
+ */
+public class UCanAccess {
+    public static String accessDbPath="access.mdb";
+    public static String url= UcanaccessDriver.URL_PREFIX+accessDbPath+";newDatabaseVersion=V2003";
+    private static final String user = "sa";
+    private static final String pass = "pass";
+    private static final String className = "net.ucanaccess.jdbc.UcanaccessDriver";
+
+    public static synchronized Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new UcanaccessDriver());//this is optional
+        return DriverManager.getConnection(url, user, pass);
+    }
+
+}
---
+++ jdbc-examples/Common/src/test/java/org/automation/db/examples/StatementExamples.java
@@ -0,0 +1,30 @@
+package org.automation.db.examples;
+
+import java.sql.Statement;
+
+/**
+ * Created by shantonu on 8/4/16.
+ * plan to show some example commonly from connection
+ */
+public class StatementExamples {
+
+    public void createTablesExample(){
+
+    }
+    public void executeQueryWithFunctions(){
+
+    }
+    public void executeQueryExample(){
+
+    }
+    public void insertDataExample(){
+
+    }
+
+    public void transaction(){
+
+    }
+    public void executeLikeExample(){
+
+    }
+}
