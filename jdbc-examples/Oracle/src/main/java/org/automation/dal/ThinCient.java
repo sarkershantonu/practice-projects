6260c37b0dcb66fc3dff24664cae0d5c8ebda17f
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-04 00:00:00 +0200

    Example

--- jdbc-examples/Oracle/src/main/java/org/automation/dal/ThinCient.java
+++ jdbc-examples/Oracle/src/main/java/org/automation/dal/ThinCient.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
  */
 public class ThinCient {
 
-    public static String url= "jdbc:oracle:oci://<HOST>:<PORT>/<DB>";
+    public static String url= "jdbc:oracle:thin://<HOST>:<PORT>/<DB>";
     private static final String user = "user";
     private static final String pass = "pass";
     private static final String className = "oracle.jdbc.driver.OracleDriver";
@@ -23,6 +23,8 @@ public class ThinCient {
     public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         Class.forName(className).newInstance();
         
-        return DriverManager.getConnection(url,user,pass);
+        return DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:mkyong", "username",
+                "password");
     }
 }
