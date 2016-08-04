c0b80f8da6f218571e47787bcfa126076cbc57e8
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-04 03:11:03 +0200

    Hive complete example

--- jdbc-examples/Apache Derby/src/main/java/org/automation/dal/DerbyGateway.java
+++ jdbc-examples/Apache Derby/src/main/java/org/automation/dal/DerbyGateway.java
@@ -1,7 +1,30 @@
 package org.automation.dal;
 
+import org.apache.derby.jdbc.ClientDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by shantonu on 7/10/16.
  */
 public class DerbyGateway {
+
+    public static String url= "jdbc:derby://<HOST>:<PORT>/<DB>;create=true";//create example jdbc:derby://localhost:1527/myDB;create=true;user=me;password=mine
+    private static final String user = "user";
+    private static final String pass = "pass";
+    private static final String className = "org.apache.derby.jdbc.ClientDriver";
+
+    public static synchronized Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new ClientDriver());
+        return DriverManager.getConnection(url, user, pass);
+    }
+    public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class.forName(className).newInstance();
+        return DriverManager.getConnection(url,user,pass);
+    }
+    public static synchronized void shutdown() throws SQLException {
+        DriverManager.getConnection("jdbc:derby:;shutdown=true");
+    }
 }
--- jdbc-examples/Apache Hive/src/main/java/org/automation/dal/HiveGateway.java
+++ jdbc-examples/Apache Hive/src/main/java/org/automation/dal/HiveGateway.java
@@ -1,20 +1,23 @@
 package org.automation.dal;
 
+import org.apache.hive.jdbc.HiveDriver;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Created by shantonu on 7/10/16.
+ * Created by shantonu on 8/4/16.
  */
-public class JdbcDB2 {
-    public static String url= "jdbc:postgresql://<HOST>:<PORT>/<DB>";
+public class HiveGateway {
+
+    public static String url= "jdbc:hive://<HOST>:<PORT>/<DB>";
     private static final String user = "user";
     private static final String pass = "pass";
-    private static final String className = "org.postgresql.Driver";
+    private static final String className = "org.apache.hive.jdbc.HiveDriver";
 
     public static synchronized Connection getConnection() throws SQLException {
-        //DriverManager.registerDriver();
+        DriverManager.registerDriver(new HiveDriver());
         return DriverManager.getConnection(url, user, pass);
     }
     public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
