ade5b820df003bf9a242b86bdc70644199192ab0
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-04 03:26:46 +0200

    Project refactored

--- jdbc-examples/DB2/src/main/java/org/automation/dal/JdbcDB2.java
+++ jdbc-examples/DB2/src/main/java/org/automation/dal/JdbcDB2.java
@@ -8,13 +8,14 @@ import java.sql.SQLException;
  * Created by shantonu on 7/10/16.
  */
 public class JdbcDB2 {
-    public static String url= "jdbc:postgresql://<HOST>:<PORT>/<DB>";
+    public static String url= "jdbc:db2://<HOST>:<PORT>/<DB>";
     private static final String user = "user";
     private static final String pass = "pass";
-    private static final String className = "org.postgresql.Driver";
+    private static final String className = "com.ibm.db2.jcc.DB2Driver";
 
     public static synchronized Connection getConnection() throws SQLException {
         //DriverManager.registerDriver();
+
         return DriverManager.getConnection(url, user, pass);
     }
     public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
