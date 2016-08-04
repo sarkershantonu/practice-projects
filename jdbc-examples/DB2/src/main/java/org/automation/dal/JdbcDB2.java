8944ce73631949ea9003e8cfbfdfb2777425b0b4
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-04 03:49:58 +0200

    DB2 without dependency libs

--- jdbc-examples/DB2/src/main/java/org/automation/dal/JdbcDB2.java
+++ jdbc-examples/DB2/src/main/java/org/automation/dal/JdbcDB2.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 /**
  * Created by shantonu on 7/10/16.
+ * // full driver list, i cound not found in maven repo, so will need to add in lib directory to include local
  */
 public class JdbcDB2 {
     public static String url= "jdbc:db2://<HOST>:<PORT>/<DB>";
@@ -13,12 +14,7 @@ public class JdbcDB2 {
     private static final String pass = "pass";
     private static final String className = "com.ibm.db2.jcc.DB2Driver";
 
-    public static synchronized Connection getConnection() throws SQLException {
-        //DriverManager.registerDriver();
-
-        return DriverManager.getConnection(url, user, pass);
-    }
-    public static synchronized Connection getLegacyConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static synchronized Connection getConnection() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         Class.forName(className).newInstance();
         return DriverManager.getConnection(url,user,pass);
     }
