d0d88117d927d73186134188bcd8029984120b0c
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-18 05:25:26 -0500

    initial project

---
+++ Bug-Storing-WS/src/main/java/org/automation/bug/ws/App.java
@@ -0,0 +1,12 @@
+package org.automation.bug.ws;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by shantonu on 12/18/16.
+ */
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
