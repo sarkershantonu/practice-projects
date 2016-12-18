13dab9641d2f1b94325b4618a257ba498d208e5b
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-18 05:25:39 -0500

    initial commit

--- Bug-Storing-WS/src/main/java/org/automation/bug/ws/App.java
+++ Bug-Storing-WS/src/main/java/org/automation/bug/ws/App.java
@@ -1,12 +1,16 @@
 package org.automation.bug.ws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by shantonu on 12/18/16.
  */
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+        System.out.println("Bug storing web service is running");
     }
 }
