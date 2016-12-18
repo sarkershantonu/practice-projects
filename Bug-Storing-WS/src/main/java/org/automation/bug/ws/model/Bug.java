e6f9162c8ba1cf1bab28cbb6890bbda999397769
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-18 06:40:24 -0500

    model

---
+++ Bug-Storing-WS/src/main/java/org/automation/bug/ws/model/Bug.java
@@ -0,0 +1,33 @@
+package org.automation.bug.ws.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.swing.text.StringContent;
+
+/**
+ * Created by shantonu on 12/18/16.
+ */
+@Entity
+public class Bug {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private String title;
+    private String description;
+
+}
