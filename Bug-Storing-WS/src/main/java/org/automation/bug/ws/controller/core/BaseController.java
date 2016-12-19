6dfd03c83ee9b871aeb6feb4136c98eb49ea1a39
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-18 21:20:30 -0500

    service complete

---
+++ Bug-Storing-WS/src/main/java/org/automation/bug/ws/controller/core/BaseController.java
@@ -0,0 +1,7 @@
+package org.automation.bug.ws.controller.core;
+
+/**
+ * Created by shantonu on 12/18/16.
+ */
+public class BaseController {
+}
---
+++ Bug-Storing-WS/src/main/java/org/automation/bug/ws/service/BugServiceBean.java
@@ -0,0 +1,7 @@
+package org.automation.bug.ws.service;
+
+/**
+ * Created by shantonu on 12/18/16.
+ */
+public class BugServiceBean {
+}
