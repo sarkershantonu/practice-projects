dbc1e2c426ddd8b40043b08fbe844d45284ec1ce
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-19 08:19:39 -0500

    project deleted

--- BugReportWS/src/main/java/org/automation/bugReport/ws/web/core/BaseController.java
+++
@@ -1,8 +0,0 @@
-package org.automation.bugReport.ws.web.core;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class BaseController {
-	protected Logger log = LoggerFactory.getLogger(getClass());
-}
