75e9cb47c4e002760f082fdde7f07f3a48a60324
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-19 08:15:24 -0500

    full bug table running..

--- Bug-Storing-WS/src/main/java/org/automation/bug/ws/model/Bug.java
+++ Bug-Storing-WS/src/main/java/org/automation/bug/ws/model/Bug.java
@@ -10,12 +10,8 @@ import javax.persistence.Id;
  */
 @Entity
 public class Bug {
-
-
     public Bug() {}
-    @Id
-    @GeneratedValue
-    private Long id;
+
     public Long getId() {
         return id;
     }
@@ -24,13 +20,97 @@ public class Bug {
         this.id = id;
     }
 
-     private String text;
-
-    public String getText() {
-        return text;
+    public String getTitle() {
+        return title;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setTitle(String title) {
+        this.title = title;
     }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getAttachmentPath() {
+        return AttachmentPath;
+    }
+
+    public void setAttachmentPath(String attachmentPath) {
+        AttachmentPath = attachmentPath;
+    }
+
+    public String getExceptions() {
+        return exceptions;
+    }
+
+    public void setExceptions(String exceptions) {
+        this.exceptions = exceptions;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getFoundDateTime() {
+        return foundDateTime;
+    }
+
+    public void setFoundDateTime(String foundDateTime) {
+        this.foundDateTime = foundDateTime;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getServility() {
+        return servility;
+    }
+
+    public void setServility(String servility) {
+        this.servility = servility;
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String summary;
+    private String Description;
+    private String AttachmentPath;
+    private String exceptions;
+    private String comments;
+    private String foundDateTime;
+    private String tags;
+    private String priority;
+    private String servility;
 }
