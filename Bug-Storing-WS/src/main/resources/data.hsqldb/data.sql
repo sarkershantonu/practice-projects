eb5a2da2e1a047321783aa55e73fb5d56f8a7486
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-18 21:28:02 -0500

    sql update

---
+++ Bug-Storing-WS/src/main/resources/data.hsqldb/data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO Bugs (text, description) VALUES ('First Bug'), ('Description of first bug ');
+INSERT INTO Bugs (text, description) VALUES ('আমার প্রথম মেসেজ'), ('Description of Bangla bug ');
