cf5cfa627ae78228fc0b9dd0374e2c7fba08240c
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-10 01:11:29 -0500

    log4j basic config

---
+++ Logging Examples/Apache Log4j2/src/main/java/org/automation/log.java
@@ -0,0 +1,14 @@
+package org.automation;
+
+import org.apache.log4j.BasicConfigurator;
+
+/**
+ * Created by shantonu on 1/10/17.
+ */
+public class log {
+    public static void main(String[] args) throws Exception{
+        BasicConfigurator.configure();
+
+    }
+
+}
