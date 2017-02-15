8d2d22f59d0d38a02aaee9f69e63ff78a43ad0eb
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-14 23:10:56 -0500

    big decimal exp

---
+++ JavaPractices/src/main/java/bigdecimal/App.java
@@ -0,0 +1,14 @@
+package bigdecimal;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by shantonu on 2/14/17.
+ */
+public class App {
+    public static void main(String[] args) {
+
+        BigDecimal num1 = new BigDecimal(1234567);
+
+    }
+}
