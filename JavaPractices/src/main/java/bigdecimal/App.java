68ca769502121776ff577853b95ba013a0f311ef
sarkershantonu <shantonu_oxford@yahoo.com>
2017-02-15 23:18:33 -0500

    round celing

--- JavaPractices/src/main/java/bigdecimal/App.java
+++ JavaPractices/src/main/java/bigdecimal/App.java
@@ -11,7 +11,12 @@ public class App {
         BigDecimal num1 = new BigDecimal(1234567);
         BigDecimal num2 = new BigDecimal(100000);
 
-        System.out.println("Plain : "+num1.divide(num2));
+        System.out.println("No Rounding : "+num1.divide(num2));
+        System.out.println("ROUND_CEILING @1 : "+num1.divide(num2, 1, BigDecimal.ROUND_CEILING));
+        System.out.println("ROUND_CEILING @2 : "+num1.divide(num2, 2, BigDecimal.ROUND_CEILING));
+        System.out.println("ROUND_CEILING @3 : "+num1.divide(num2, 3, BigDecimal.ROUND_CEILING));
+        System.out.println("ROUND_CEILING @4 : "+num1.divide(num2, 4, BigDecimal.ROUND_CEILING));
+        System.out.println("ROUND_CEILING @5 : "+num1.divide(num2, 5, BigDecimal.ROUND_CEILING));
 
     }
 }
